6a06abe23dc78a09765ff9be916bff92a39117b7
William Fong <willfong@mit.edu>
2026-04-28 11:02:54 -0600

    moving over to pyproject.toml setups

--- src/cuvarbase/__init__.py
+++ src/cuvarbase/__init__.py
@@ -1,3 +1,4 @@
-# import pycuda.autoinit causes problems when running e.g. FFT
-import pycuda.autoprimaryctx
 __version__ = "0.3.0"
+
+# import pycuda.autoinit causes problems when running e.g. FFT
+import pycuda.autoprimaryctx  # noqa: E402, F401
